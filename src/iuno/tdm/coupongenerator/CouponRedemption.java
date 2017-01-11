e7a222316cf7c6d31c519116ce9fcd1b9a9aea46
Hans-Peter Bock (TW552bo) <hans-peter.bock@de.trumpf.com>
2017-01-11 11:48:13 +0100

    added empty new class

---
+++ src/iuno/tdm/coupongenerator/CouponRedemption.java
@@ -0,0 +1,28 @@
+package iuno.tdm.coupongenerator;
+
+/**
+ * Copyright 2017 TRUMPF Werkzeugmaschinen GmbH + Co. KG
+ * Created by Hans-Peter Bock on 11.01.2017.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This class expects a private key, e.g. from a paper wallet, and shall
+ * send all bitcoins available for this private key to a provided bitcoin
+ * address.
+ */
+public class CouponRedemption {
+
+}
